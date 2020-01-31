a4f1c9691c3c3fed7ce6ad9bda3e36d02c83d8d0
ryankayne <coope1rk@gmail.com>
2020-01-31 11:00:06 -0800

    MVP

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,3 +2,25 @@ const request = require('supertest');
 
 const server = require('./server.js');
 
+describe('server', function() {
+    it('runs the test', function() {
+        expect(true).toBe(true);
+    })
+
+
+    describe('GET /', function() {
+        it('should return 200', () => {
+            return request(server).get('/')
+                .then(res => {
+                expect(res.status).toBe(200);
+            })
+        })
+
+        it('should return JSON', function() {
+            return request(server).get('/')
+                .then(res => {
+                expect(res.type).toMatch(/json/i);
+            })
+        })
+    })
+})
